124358f3e3674e837a3dbd26e285024e7210232b
gtalarico <gtalarico@gmail.com>
2017-06-08 00:42:27 -0300

    Merged Init - Revit Host

--- rpw/__init__.py
+++ rpw/__init__.py
@@ -26,79 +26,33 @@ Copyright 2017 Gui Talarico
 """
 
 __title__ = 'revitpythonwrapper'
-__version__ = '0.0.98'
+__version__ = '1.0.0'
 __maintainer__ = 'Gui Talarico'
 __license__ = 'MIT'
 __contact__ = 'github.com/gtalarico/revitpythonwrapper'
 
-import sys
-from rpw.utils.logger import logger
+if __name__ == 'make.doc':
+    pass
+print __name__
+from revit import revit, DB, UI
 
-try:
-    #  This is a workaround to fix Sphinx's autodoc
-    import clr
-    clr.AddReference('RevitAPI')
-    clr.AddReference('RevitAPIUI')
-    clr.AddReference('System')
+# from rpw.base import BaseObjectWrapper
+# from rpw.element import Element, Instance, Symbol, Family, Category
+# from rpw.element import WallInstance, WallSymbol, WallFamily, WallCategory
+# from rpw.element import Room, Area, AreaScheme
+# from rpw.geometry import Point, PointCollection
+# from rpw.parameter import ParameterSet, Parameter
+# from rpw.selection import Selection
+# from rpw.collector import Collector, ParameterFilter
+# from rpw.transaction import Transaction, TransactionGroup
+# from rpw.utils.logger import logger
+# from rpw.enumeration import BipEnum, BicEnum
+# from rpw.utils.coerce import to_element_ids, to_elements
 
-    from Autodesk.Revit import DB
-    from Autodesk.Revit import UI
-    from System.Collections.Generic import List
-    from System import Enum
+# try:
+    # import forms
+# except ImportError as errmsg:
+    # logger.critical('Could Not load Forms dependencies')
+    # logger.warning(errmsg)
 
-except Exception as errmsg:
-    logger.error(errmsg)
-    platform = {'testing': True}
-    logger.warning('Could not Revit Document. Will Import Sphinx Compat Vars')
-    from rpw.utils.sphinx_compat import *
-
-# Imported Revit's Assemblies
-else:
-    try:
-        uidoc = __revit__.ActiveUIDocument
-        doc = __revit__.ActiveUIDocument.Document
-        version = __revit__.Application.VersionNumber.ToString()
-        platform = {'revit': version}
-        logger.debug("Running In Revit")
-
-    except NameError:
-        logger.debug('Could not find pyRevit Document. Trying Dynamo.')
-        try:
-            clr.AddReference("RevitServices")
-        except:
-            raise Exception('Could Not Find Dynamo Services')
-        else:
-            # Adds Built in Library for Dynamo
-            sys.path.append(r'C:\Program Files (x86)\IronPython 2.7\Lib')
-
-            import RevitServices
-            from RevitServices.Persistence import DocumentManager
-            from RevitServices.Transactions import TransactionManager
-            doc = DocumentManager.Instance.CurrentDBDocument
-            uiapp = DocumentManager.Instance.CurrentUIApplication
-            app = uiapp.Application
-            uidoc = DocumentManager.Instance.CurrentUIApplication.ActiveUIDocument
-            version = app.VersionNumber.ToString()
-            platform = {'dynamo': version}
-            logger.info('Running in Dynamo')
-
-
-if platform is not None or platform.get('testing'):
-    from rpw.base import BaseObjectWrapper
-    from rpw.element import Element, Instance, Symbol, Family, Category
-    from rpw.element import WallInstance, WallSymbol, WallFamily, WallCategory
-    from rpw.element import Room, Area, AreaScheme
-    from rpw.geometry import Point, PointCollection
-    from rpw.parameter import ParameterSet, Parameter
-    from rpw.selection import Selection
-    from rpw.collector import Collector, ParameterFilter
-    from rpw.transaction import Transaction, TransactionGroup
-    from rpw.utils.logger import logger
-    # from rpw.enumeration import BipEnum, BicEnum
-    # from rpw.utils.coerce import to_element_ids, to_elements
-
-    try:
-        import forms
-    except ImportError as errmsg:
-        logger.critical('Could Not load Forms dependencies')
-        logger.warning(errmsg)
+# from rpw.utils.sphinx_compat import *
---
+++ rpw/revit.py
@@ -0,0 +1,120 @@
+import clr
+clr.AddReference('System')
+from System.Diagnostics import Process
+# from rpw.utils.logger import logger
+
+
+class Revit:
+    """Revit Application Wrapper """
+
+    class HOSTS():
+        RPS = 'RPS'
+        DYNAMO = 'Dynamo'
+
+    def __init__(self):
+        try:
+            self.uiapp = __revit__
+            self.host = Revit.HOSTS.RPS
+        except NameError:
+            try:
+                self.uiapp = self.find_dynamo_uiapp()
+            except Exception as errmsg:
+                # logger.error(errmsg)
+                print(errmsg)
+                raise ImportError('A Revit Application handle could not be found')
+        clr.AddReference('RevitAPI')
+        clr.AddReference('RevitAPIUI')
+
+    def find_dynamo_uiapp(self):
+        clr.AddReference("RevitServices")
+        import RevitServices
+        from RevitServices.Persistence import DocumentManager
+
+        self.host = Revit.HOSTS.DYNAMO
+        import sys
+        sys.path.append(r'C:\Program Files (x86)\IronPython 2.7\Lib')
+
+        return DocumentManager.Instance.CurrentUIApplication
+
+    @property
+    def doc(self):
+        return self.uiapp.ActiveUIDocument.Document
+
+    @property
+    def uidoc(self):
+        return self.uiapp.ActiveUIDocument
+
+    @property
+    def app(self):
+        return self.uiapp.Application
+
+    @property
+    def docs(self):
+        return self.app.Application.Documents
+
+    @property
+    def username(self):
+        return self.uiapp.Application.Username
+
+    @property
+    def version(self):
+        return RevitVersion(self.uiapp)
+
+    @property
+    def process(self):
+        return Process.GetCurrentProcess()
+
+    @property
+    def process_id(self):
+        return Process.GetCurrentProcess().Id
+
+    @property
+    def process_name(self):
+        return Process.GetCurrentProcess().ProcessName
+
+    def __repr__(self):
+        return '<{version} [{process}:{pid}] >'.format(version=self.version,
+                                                       process=self.process_name,
+                                                       pid=self.process_id)
+
+    # @property
+    # def proc_screen(self):
+    #     clr.AddReferenceByPartialName('System.Windows.Forms')
+    #     # noinspection PyUnresolvedReferences
+    #     from System.Windows.Forms import Screen
+    #     return Screen.FromHandle(Process.GetCurrentProcess().MainWindowHandle)
+    #
+    # def is_newer_than(self, version):
+    #     return int(self.version) > int(version)
+    #
+    # def is_older_than(self, version):
+    #     return int(self.version) < int(version)
+
+class RevitVersion():
+    def __init__(self, uiapp):
+        self.uiapp = uiapp
+
+    @property
+    def year(self):
+        return self.uiapp.Application.VersionNumber
+
+    @property
+    def name(self):
+        return self.uiapp.Application.VersionName
+
+    @property
+    def build(self):
+        return self.uiapp.Application.VersionBuild
+
+    def __lt__(self):
+        """ Handle Version Comparison Logic"""
+        raise NotImplemented
+
+    def __repr__(self):
+        return '<Version: {year}: {build}>'.format(year=self.name,
+                                                   build=self.build)
+    def __str__(self):
+        return '{name}:{build}'.format(name=self.name, build=self.build)
+
+revit = Revit()
+from Autodesk.Revit import DB, UI
