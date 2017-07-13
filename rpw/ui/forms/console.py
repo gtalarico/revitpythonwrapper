e3fd568839fc84107180dc72c942e1f2c1428318
Gui Talarico <gtalarico@gmail.com>
2017-07-13 16:19:38 -0400

    Removed Terminate Button.
    Need to add Console Wrapper otherwise it crashes revit.

--- rpw/ui/forms/console.py
+++ rpw/ui/forms/console.py
@@ -64,17 +64,17 @@ class Console(Window):
                         <RowDefinition Height="*"></RowDefinition>
                     </Grid.RowDefinitions>
                     <TextBox Grid.Column="1" Grid.Row="1"  HorizontalAlignment="Stretch"
-                             Name="tbox" Margin="6,6,6,42" VerticalAlignment="Stretch"
+                             Name="tbox" Margin="6,6,6,6" VerticalAlignment="Stretch"
                              AcceptsReturn="True" VerticalScrollBarVisibility="Auto"
                              TextWrapping="Wrap"
                              FontFamily="Consolas" FontSize="12.0"
                              />
-                     <Button Content="Terminate" Margin="6,6,6,6" Height="30"
-                             Grid.Column="1" Grid.Row="1" VerticalAlignment="Bottom"
-                             Click="terminate"></Button>
                 </Grid>
                 </Window>
     """
+    # <Button Content="Terminate" Margin="6,6,6,6" Height="30"
+    #     Grid.Column="1" Grid.Row="1" VerticalAlignment="Bottom"
+    #     Click="terminate"></Button>
 
     CARET = '>>> '
 
